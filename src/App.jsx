963e197ceb110da01dfec20ca2bd6ee7364e0926
Tiagotpk <ttrpk@hotmail.com>
2024-07-17 16:09:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import Game from "./components/Game";
+import NewGameForm from "./components/NewGameForm";
+import useGameCollection from "./hooks/useGameCollection";
+
+function App() {
+  const {games, addGame, removeGame} = useGameCollection()
+  
+  return (
+    <div className="app">
+      <h1>Biblioteca de Jogos</h1>
+      <NewGameForm addGame={addGame} />
+      <div className="games">
+        {games.length > 0
+          ? games.map((game) => (
+            <Game 
+            key={game.id}
+            title={game.title}
+            cover={game.cover}
+            onRemove={() => removeGame(game.id)}
+             />))
+             : (
+              <h2>Parece que não tem nada adicionado ainda, tente adicionar alguns jogos...</h2>
+             )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
